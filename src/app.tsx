f4cb4fef563c9a20e24f8b89b80d77891e6c04ef
Martynas Žilinskas <martynas@quatrodev.com>
2018-08-16 12:00:24 +0300

    Added Hello World react component.

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class App extends React.Component {
+    public render(): JSX.Element {
+        return <div>Hello World!</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
